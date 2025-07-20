01bf9453353e635f0a366e7428568db3999df33d
Nabin Adhikari <78608299+nabinadhikari96@users.noreply.github.com>
2025-07-21 03:59:14 +0400

    Add .next/static/chunks/src_app_challenges_page_tsx_11f632dd._.js 1753055955575

---
+++ .next/static/chunks/src_app_challenges_page_tsx_11f632dd._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_44bf0a81._.js",
+  "static/chunks/src_97b1a646._.js"
+],
+    source: "dynamic"
+});
